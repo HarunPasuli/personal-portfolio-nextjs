3b72d812b2496d63426688e49cdc7f2e1edbb5ad
HarunPasuli <harunpasuli24@gmail.com>
2024-01-02 15:22:05 +0100

    removed ids

--- app/page.jsx
+++ app/page.jsx
@@ -4,11 +4,11 @@ import Projects from "@/components/Projects";
 
 const Home = () => {
 	return (
-		<>
+		<div className="flex flex-col items-center w-full">
 			<Hero />
 			<About />
 			<Projects />
-		</>
+		</div>
 	);
 };
 
